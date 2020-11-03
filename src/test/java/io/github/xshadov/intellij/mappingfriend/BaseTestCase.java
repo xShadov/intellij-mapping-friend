eb08267854d0da4d9a07f0941bb0dcb183897cd2
xShadov <shadov.profile@gmail.com>
2020-11-03 22:11:43 +0100

    Added mock JDK - psiClass was sometimes null because of some JDK related issues - solution found in https://github.com/mplushnikov/lombok-intellij-plugin

--- src/test/java/io/github/xshadov/intellij/mappingfriend/BaseTestCase.java
+++ src/test/java/io/github/xshadov/intellij/mappingfriend/BaseTestCase.java
@@ -1,7 +1,16 @@
 package io.github.xshadov.intellij.mappingfriend;
 
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.projectRoots.JavaSdk;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.roots.ContentEntry;
+import com.intellij.openapi.roots.LanguageLevelModuleExtension;
+import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
+import com.intellij.pom.java.LanguageLevel;
+import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.testFramework.PsiTestUtil;
+import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 import com.intellij.util.PathUtil;
 
@@ -26,5 +35,20 @@ public abstract class BaseTestCase extends LightPlatformCodeInsightFixtureTestCa
 	@Override
 	protected String getTestDataPath() {
 		return "src/test/resources/testData";
+	}
+
+	@Override
+	protected LightProjectDescriptor getProjectDescriptor() {
+		return new DefaultLightProjectDescriptor() {
+			@Override
+			public Sdk getSdk() {
+				return JavaSdk.getInstance().createJdk("java 1.8", "lib/mockJDK-1.8", false);
+			}
+
+			@Override
+			public void configureModule(Module module, ModifiableRootModel model, ContentEntry contentEntry) {
+				model.getModuleExtension(LanguageLevelModuleExtension.class).setLanguageLevel(LanguageLevel.JDK_1_8);
+			}
+		};
 	}
 }
